d691a608bed349952ac34e30132e891ee1229210
palepanorama <matthew.keith.thomas@gmail.com>
2020-10-28 09:39:02 -0500

    adds error erb and updates userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,13 @@ class UsersController < ApplicationController
     end 
 
     post '/login' do 
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id 
+            redirect '/instruments'
+        else 
+            redirect '/error'
+        end 
     end 
 
     get '/signup' do 
@@ -12,5 +19,16 @@ class UsersController < ApplicationController
     end 
 
     post '/signup' do 
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        if @user.username != "" && @user.save 
+            redirect '/login'
+        else 
+            redirect 'error'
+        end
     end 
+
+    get '/error' do 
+        erb :error 
+    end 
+
 end 
