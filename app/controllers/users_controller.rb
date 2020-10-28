8238c216cfb5f567b4f660912ef3f2497ec237b9
palepanorama <matthew.keith.thomas@gmail.com>
2020-10-28 09:50:56 -0500

    fixes user migration so now post request for signup is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,13 @@ class UsersController < ApplicationController
 
     post '/signup' do 
         @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-        if @user.username != "" && @user.save 
-            redirect '/login'
+        if @user 
+            @user.save 
+            session[:user_id] = @user.id 
+            redirect '/instruments/user:id'
         else 
-            redirect 'error'
-        end
+            redirect '/error'
+        end 
     end 
 
     get '/error' do 
--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20201027133844) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
